e03eb4b40196abadae9e5d23caff7953c68ec304
MrMultiDios <mrmultinoob@gmail.com>
2026-03-16 10:27:56 -0500

    Update start.py

--- start.py
+++ start.py
@@ -3,8 +3,9 @@ import os
 import configparser
 from waitress import serve
 from contact import main
-from contact.model import Base
+from contact.model import Base, Subject
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 config = configparser.ConfigParser()
 config.read('development.ini')
@@ -17,6 +18,15 @@ if database_url:
 # Crear tablas automáticamente
 engine = create_engine(settings['sqlalchemy.url'])
 Base.metadata.create_all(engine)
+
+# Poblar subjects si no existen
+Session = sessionmaker(bind=engine)
+session = Session()
+if session.query(Subject).count() == 0:
+    for name in ('Python', 'Pyramid', 'Pygame'):
+        session.add(Subject(name=name))
+    session.commit()
+session.close()
 
 app = main({}, **settings)
 port = int(os.environ.get('PORT', 6543))
