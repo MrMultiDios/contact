b81a7ed815fb53413773b561db64f3240f64cd30
MrMultiDios <mrmultinoob@gmail.com>
2026-03-16 10:23:58 -0500

    Update start.py

--- start.py
+++ start.py
@@ -3,15 +3,20 @@ import os
 import configparser
 from waitress import serve
 from contact import main
+from contact.model import Base
+from sqlalchemy import create_engine
 
 config = configparser.ConfigParser()
 config.read('development.ini')
 settings = dict(config['app:main'])
 
-# Usar DATABASE_URL de Railway si existe
 database_url = os.environ.get('DATABASE_URL')
 if database_url:
     settings['sqlalchemy.url'] = database_url
+
+# Crear tablas automáticamente
+engine = create_engine(settings['sqlalchemy.url'])
+Base.metadata.create_all(engine)
 
 app = main({}, **settings)
 port = int(os.environ.get('PORT', 6543))
