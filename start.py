83d914244fde37ac1b614e01ed4df873572dfead
MrMultiDios <mrmultinoob@gmail.com>
2026-03-16 10:12:18 -0500

    Update start.py

--- start.py
+++ start.py
@@ -7,6 +7,12 @@ from contact import main
 config = configparser.ConfigParser()
 config.read('development.ini')
 settings = dict(config['app:main'])
+
+# Usar DATABASE_URL de Railway si existe
+database_url = os.environ.get('DATABASE_URL')
+if database_url:
+    settings['sqlalchemy.url'] = database_url
+
 app = main({}, **settings)
 port = int(os.environ.get('PORT', 6543))
 print(f'Serving on port {port}')
